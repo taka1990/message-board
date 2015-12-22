8ca769f91e0d19f7f4715118542a006fba45ac00
takanoriasada <0212ta@gmail.com>
2015-12-22 00:31:30 +0000

    first commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,23 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     @message = Message.new
     # Messageをすべて取得する。
     @messages = Message.all
   end
- 
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -23,3 +36,6 @@ class MessagesController < ApplicationController
   end
   ## ここまで  
 end
+  def set_message
+    @message = Message.find(params[:id])
+  end
